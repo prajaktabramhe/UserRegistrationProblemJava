5550ef685f95caf31fe9db019ea209254f1dd8e7
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-17 20:17:55 +0530

    [Prajakta]: refactor code. wrote scanner class outside the method

--- src/useregistration/UserRegistration.java
+++ src/useregistration/UserRegistration.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.lang.*;
 public class UserRegistration
 {
+    private static final Scanner scan = new Scanner(System.in);
     public static  void main(String[] args)
     {
         //calling method to validate first name
@@ -11,11 +12,11 @@ public class UserRegistration
     //Define method to validate first name
     public static void validateUserData()
     {
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter Your First name");
         String name = scan.next();
         System.out.println("The first name is: " + name);
         String pattern = "^([A-Z]{1})([a-z]{2,})$";
+
         if( name.matches(pattern))
         {
             System.out.println("Name is Valid");
