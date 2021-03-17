a1667ae0768ba40512f9a97e3ce1ab7e2b18f561
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-17 21:54:24 +0530

    [Prajakta]: validated the mobile number using regex pattern

--- src/useregistration/UserRegistration.java
+++ src/useregistration/UserRegistration.java
@@ -4,19 +4,23 @@ import java.lang.*;
 public class UserRegistration
 {
     private static final Scanner scan = new Scanner(System.in);
-
     public static  void main(String[] args)
     {
         //calling method to validate first Name
         validateUserData();
-        //calling method to validate Last Name
+        //calling method to validate Last  Name
         validateUserLastName();
+        //calling method to validate Email Id
+        validEmail();
+        //calling method to validate Mobile No
+        validMobileNo();
     }
     //Define method to validate first name
     public static void validateUserData()
     {
         System.out.println("Enter Your First name");
-        String name = scan.next();
+        String name = scan.nextLine();
+
         System.out.println("The first name is: " + name);
         String pattern = "^([A-Z]{1})([a-z]{2,})$";
         if( name.matches(pattern))
@@ -32,10 +36,10 @@ public class UserRegistration
     public static void validateUserLastName()
     {
         System.out.println("Enter Your Last name");
-        String name = scan.next();
+        String name = scan.nextLine();
+
         System.out.println("The Last name is: " + name);
         String pattern = "^([A-Z]{1})([a-z]{2,})$";
-
         if( name.matches(pattern))
         {
             System.out.println("Name is Valid");
@@ -46,5 +50,39 @@ public class UserRegistration
         }
     }
 
+    //Define method to validate email id
+    public static void validEmail()
+    {
+        System.out.println("Enter Your Email");
+        String name = scan.nextLine();
 
+        System.out.println("The Email is: " + name);
+        String pattern = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
+
+    //Define method to validate Mobile No
+    public static void validMobileNo()
+    {
+        System.out.println("Enter Your Mobile No.");
+        String name = scan.nextLine();
+
+        System.out.println("The Mobile No is: " + name);
+        String pattern =  "^([0-9]{2})[ ]([1-9]{1}[0-9]{9})$";
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
 }
