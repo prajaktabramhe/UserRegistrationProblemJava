a460215aa2ba065a4c27741f25614ac841963c34
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-17 19:39:50 +0530

    [Prajakta]: validated first name with regex pattern

---
+++ src/useregistration/UserRegistration.java
@@ -0,0 +1,28 @@
+package useregistration;
+import java.util.Scanner;
+import java.lang.*;
+public class UserRegistration
+{
+    public static  void main(String[] args)
+    {
+        //calling method to validate first name
+        validateUserData();
+    }
+    //Define method to validate first name
+    public static void validateUserData()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Your First name");
+        String name = scan.next();
+        System.out.println("The first name is: " + name);
+        String pattern = "^([A-Z]{1})([a-z]{2,})$";
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
+}
